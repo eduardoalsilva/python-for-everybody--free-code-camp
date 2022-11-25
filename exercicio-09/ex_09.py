bdd94d6175f97e50fbe66f083f763475abdceaa2
Eduardo Silva <eduardoalsilva45@gmail.com>
2022-11-25 00:23:21 -0300

    Comentando os códigos e alterando nome de variáveis do exercício 09

--- exercicio-09/ex_09.py
+++ exercicio-09/ex_09.py
@@ -1,21 +1,33 @@
+# save in a variable a value wrote by the user
+# If the user doesn't write anything it saves the clown.txt file to fileName
+
 fileName = input('Enter File: ')
 if len(fileName) < 1:
     fileName = 'clown.txt'
 
+
+# save the file content in handleContent
+
 handleContent = open(fileName)
 
-di = dict( )
+
+# create a dictionary type variable called counts
+# delete extra spaces and split each line to a list variable called words
+
+counts = dict( )
 for line in handleContent:
     line = line.rstrip()
     words = line.split()
-    for w in words:
-        di[w] = di.get(w,0) + 1
+    for word in words: # counts how many the current word appears
+        counts[word] = counts.get(word,0) + 1
 
     largest = -1
     theword = None
-    for k,v in di.items():
-        if v > largest:
-            largest = v
-            theword = k
+    for keys,values in counts.items(): # compares the dictionary keys to find which one has a higher value, which one appears more
+        if values > largest:            # save the key and the value of the result to new variables
+            largest = values
+            theword = keys
+
+# prints the word and how many time it appears
 
 print('Done',theword, largest)
